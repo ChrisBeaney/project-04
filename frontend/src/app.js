7c3e0f7d2e3b83711f5770860c4786cd9588fbd8
Chris Beaney <chrisbeaney@gmail.com>
2019-09-06 16:00:28 +0100

    added frontend directory, babel.rc, package.json, webpack.config.ks

---
+++ frontend/src/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+class App extends React.Component {
+  componentDidMount() {
+    axios.get('/api/movies/')
+      .then(res => console.log(res.data))
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hello Django!</h1>
+        <h2>Project-04</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
