b6bddf7bf960ccd057b05ddf43de8f9e7ea34579
Chris Beaney <chrisbeaney@gmail.com>
2019-09-08 16:48:24 +0100

    fixed project/urls

--- project/urls.py
+++ project/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
-    # 'admin/' route not working.
-    path('admin/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('rest_framework.urls')),
     path('api/', include('jwt_auth.urls')),
-    path('holes/', include('rounds.urls')),
-    path('', include('frontend.urls')),
+    path('api/', include('rounds.urls')),
+    path('', include('frontend.urls'))
 ]
